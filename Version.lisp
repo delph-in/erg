06d00717b5206d402ff33b3d9b4a19755486f551
Stephan Oepen <oepen@users.noreply.github.com>
2000-05-22 22:17:32 +0000

    minor revision to improve aesthetics.
    git-svn-id: http://svn.delph-in.net/erg/trunk@821 3df82f5b-d43a-0410-af33-fce91db48ec5

--- Version.lisp
+++ Version.lisp
@@ -5,4 +5,5 @@
 ;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
 ;;;
 
-(defparameter *grammar-version* "LinGO (may00)")
+(defparameter *grammar-version* "LinGO (eubp)")
+
