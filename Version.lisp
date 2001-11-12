08888e590d060ce83e413258a094f30638882de7
Dan Flickinger <danflick@users.noreply.github.com>
2001-11-12 05:31:22 +0000

    Long-overdue grammar which behaves well with generation
    git-svn-id: http://svn.delph-in.net/erg/trunk@854 3df82f5b-d43a-0410-af33-fce91db48ec5

--- Version.lisp
+++ Version.lisp
@@ -5,5 +5,4 @@
 ;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
 ;;;
 
-(defparameter *grammar-version* "LinGO (17-jul-01)")
-
+(defparameter *grammar-version* "LinGO (12-Nov-01)")
