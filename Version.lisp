c81757d9d482e4326b18606a34786d738ad8c3f9
Dan Flickinger <danflick@users.noreply.github.com>
2002-07-09 17:32:58 +0000

    Improved semantics for generation, again
    git-svn-id: http://svn.delph-in.net/erg/trunk@857 3df82f5b-d43a-0410-af33-fce91db48ec5

--- Version.lisp
+++ Version.lisp
@@ -5,6 +5,6 @@
 ;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
 ;;;
 
-(defparameter *grammar-version* "LinGO (16-Apr-02)")
+(defparameter *grammar-version* "LinGO (09-Jul-02)")
 
 
