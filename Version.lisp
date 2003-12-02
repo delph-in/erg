f49173ac7896ca300fc1e31d44049f51892a82ea
Dan Flickinger <danflick@users.noreply.github.com>
2003-12-02 14:21:08 +0000

    More consistency for prednames for SEM-*
    git-svn-id: http://svn.delph-in.net/erg/trunk@980 3df82f5b-d43a-0410-af33-fce91db48ec5

--- Version.lisp
+++ Version.lisp
@@ -5,4 +5,4 @@
 ;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
 ;;;
 
-(defparameter *grammar-version* "LinGO (01-Dec-03)")
+(defparameter *grammar-version* "LinGO (02-Dec-03)")
