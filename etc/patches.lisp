7d3f778c177cb740fd7c897c2a7375f8ead57467
Stephan Oepen <oepen@users.noreply.github.com>
2016-04-19 22:38:49 +0000

    a release candidate for the 1214 SEMI: now (i hope) with all the hierarchy patches suggested by dan and formatted and broken up mostly following preferences by woodley
    git-svn-id: http://svn.delph-in.net/erg/tags/1214@23863 3df82f5b-d43a-0410-af33-fce91db48ec5

--- etc/patches.lisp
+++ etc/patches.lisp
@@ -4,23 +4,35 @@
 ;;; the pieces of an input semantics to realization to the intial generator
 ;;; chart.
 ;;; 
-;;; + block foo --- prevent descend into (grammar-internal) sub-types of .foo.
-;;; + alias foo bar --- (expose in the SEMI-I and) rename type .bar. as .foo.
-;;; + parent
+;;; + alias predicate type
+;;; + block predicate
+;;; + link type type
+;;; + parent predicate predicate
 ;;;
 ((:block "card")
  (:block "comp")
  (:block "dofm")
+ (:block "every_q")
  (:block "interval_p_end")
  (:block "numbered_hour")
  (:block "plus")
+ (:block "some_q")
  (:block "temp")
+ (:block "time_n")
  (:block "times")
  (:block "v_event")
  (:alias "exist_q" def_udef_some_a_q_rel)
+ (:link def_or_demon_q_rel def_udef_some_a_q_rel)
+ (:link def_or_proper_q_rel def_udef_some_a_q_rel)
+ (:link basic_free_relative_q_rel def_udef_some_a_q_rel)
  (:parent "_all_q" "universal_q")
  (:parent "_each_q" "universal_q")
  (:parent "every_q" "universal_q")
  (:parent "_both_q" "universal_q")
- (:parent "_either_q" "universal_q"))
+ (:parent "_either_q" "universal_q")
+ (:parent "_across_p_temp" "_across_p")
+ (:parent "_at_p_temp" "_at_p")
+ (:parent "_by_p_temp" "_by_p")
+ (:parent "_in_p_temp" "_in_p")
+ (:parent "_on_p_temp" "_on_p"))
  
