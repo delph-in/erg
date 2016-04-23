ffb083877e93b6b1af48a1f0ba27f3ab711ed376
Stephan Oepen <oepen@users.noreply.github.com>
2016-04-23 22:54:13 +0000

    some updates, taking into account comments by emily
    git-svn-id: http://svn.delph-in.net/erg/tags/1214@23897 3df82f5b-d43a-0410-af33-fce91db48ec5

--- etc/patches.lisp
+++ etc/patches.lisp
@@ -9,6 +9,17 @@
 ;;; + link type type
 ;;; + parent predicate predicate
 ;;;
+;;; _fix_me_
+;;; ? sps-descend(): cop_id > person_name.
+;;; ? sps-descend(): cop_id > be_v_prd-or-id.
+;;; ? sps-descend(): cop_id > be_v_prd-or-nv.
+;;; ? sps-descend(): excl > polite.
+;;; ? what really is ‘e_event’ (it seems to crop in from at least one entity)
+;;; ? how do ‘def_explicit_q’ and ‘def_implicit_q’ sneak in
+;;; ? embed-semi(): minute < card.
+;;; + minimize parents: ‘_at_p_temp’ has both ‘_at_p’ and ‘unspec_loc’
+;;; ? expose ‘temp_loc’, ‘dir’, and ‘state_loc’
+;;;
 ((:block "card")
  (:block "comp")
  (:block "dofm")
@@ -21,7 +32,7 @@
  (:block "time_n")
  (:block "times")
  (:block "v_event")
- (:alias "exist_q" def_udef_some_a_q_rel)
+ (:alias "existential_q" def_udef_some_a_q_rel)
  (:link def_or_demon_q_rel def_udef_some_a_q_rel)
  (:link def_or_proper_q_rel def_udef_some_a_q_rel)
  (:link basic_free_relative_q_rel def_udef_some_a_q_rel)
@@ -35,4 +46,25 @@
  (:parent "_by_p_temp" "_by_p")
  (:parent "_in_p_temp" "_in_p")
  (:parent "_on_p_temp" "_on_p"))
- 
+
+#+:null
+(progn
+  (setf semi
+    (construct-semi       
+     :ids t :rules t :descendp t :embedp t
+     :semi (read-semi
+            "~/src/logon/lingo/erg/etc/erg.smi"
+            :includep nil :finalizep nil :recordp nil)
+     :patches "~/src/logon/lingo/erg/etc/patches.lisp"
+     :finalizep t))
+  (print-semi
+   semi :stream "~/src/logon/lingo/erg/etc/hierarchy.smi"
+   :format :hierarchy)
+  (print-semi
+   semi :stream "~/src/logon/lingo/erg/etc/abstract.smi"
+   :format :compact :filter "^[^_]")
+  (print-semi
+   semi :stream "~/src/logon/lingo/erg/etc/surface.smi"
+   :format :compact :filter "^_"))
+
+
