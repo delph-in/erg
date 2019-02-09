b23e60cde0061ec91f98fe601e57dbb015449b7f
Stephan Oepen <oepen@users.noreply.github.com>
2019-02-09 21:12:48 +0000

    backpropagate (if you will) increased autonomy for dan (see revision 27383) into the 2018 release
    git-svn-id: http://svn.delph-in.net/erg/tags/2018@27384 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ tsdb/skeletons/lpp/Index.lisp
@@ -0,0 +1,2 @@
+(((:path . "lppt") (:content . "Le Petit Prince (IAA Trial)"))
+ ((:path . "lpps") (:content . "Le Petit Prince (IAA Sample)")))
---
+++ tsdb/skeletons/wdc/Index.lisp
@@ -0,0 +1,2 @@
+(((:path . "wlb03") (:content . "WDC Linux Blogs (Section 3)"))
+ ((:path . "wnb03") (:content . "EDC NLP Blogs (Section 3")))
